6fe41c98b028f02e93345508f2f6d192b1c7787a
dudleyinc3 <dudleyinc3@gmail.com>
2017-11-29 05:19:56 +0800

    create initial functionality

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-function onReady(){
-	const addToDoForm =document.getElementById('addToDoForm');
-	addToDoForm.addEventListener('submit', event => {
-	event.preventDefault();
-	let title = newToDoText.value;
-	let newLi = document.createElement('li');
-	let checkbox = document.createElement('input');
-	checkbox.type = 'checkbox';
-	});
+
+window.onload = function onReady(){
+	const addToDoForm = document.getElementById('addToDoForm');
 	const newToDoText = document.getElementById('newToDoText');
 	const toDoList = document.getElementById('toDoList');
+
+		addToDoForm.addEventListener('submit', event => {
+			event.preventDefault();
+			let title = newToDoText.value;
+			let newLi = document.createElement('li');
+			let checkbox = document.createElement('input');
+			checkbox.type = "checkbox";
+			newLi.textContent = title;
+			newLi.appendChild(checkbox);
+			toDoList.appendChild(newLi);
+			newToDoText.value = '';
+	});
 }
 
