bce25e36693c7e0c829899566a7a0e654f11b9f3
dudleyinc3 <dudleyinc3@gmail.com>
2017-12-28 00:08:58 +0800

    delete btn attempts

--- app.js
+++ app.js
@@ -77,18 +77,23 @@
 		renderTheUI();
 	}
 */
+
+
+
 function onReady(){
 	let toDos = [];
 	let id = 0;
+
 	const addToDoForm = document.getElementById('addToDoForm');
 	const newToDoText = document.getElementById('newToDoText');
 	const toDoList = document.getElementById('toDoList');
 
 	function createNewToDo(){
-
-		if (!newToDoText.value) {return; }
+	let title = newToDoText.value;
+		if (!newToDoText.value) {return;}
 		for (var i = 0; i < toDos.length; i++){
 			id = toDos[i];
+			//alert([i]);  //** it is incrementing with ea. todo
 		}
 		toDos.push({
 			title: newToDoText.value,
@@ -109,7 +114,7 @@ function onReady(){
 		toDos.forEach(function(toDo) {
 			const newLi = document.createElement('li');
 			const checkbox = document.createElement('input');
-			const deleteBtn = document.createElement('button')
+			const deleteBtn = document.createElement('button');
 			deleteBtn.innerHTML = '<span>Delete</span>';
 			checkbox.type = "checkbox";
 			
@@ -120,9 +125,40 @@ function onReady(){
 			newLi.appendChild(checkbox);
 			newLi.appendChild(deleteBtn);
 
-			deleteBtn.addEventListener('click', function deleteToDo(id){
 
-				alert('works');
+			
+			
+
+			deleteBtn.addEventListener('click', event => {
+
+				//alert('works');
+				function deleteToDo(id){
+				toDo = toDos.filter(event => toDo.id !== id);
+			}
+				
+				deleteToDo(toDo.id);
+
+
+				renderTheUI();
+
+
+				// REMOVES CHECKMARK INPUT??
+
+				/*Create a deleteToDo function that accepts an id 
+				as a parameter. In this function, remove the to-do 
+				from the ToDos array using the .filter() array method.
+				Pass .filter() a callback function that compares the id
+				of each item in the to-do list with the id parameter. 
+				The callback function should return true if the IDs don't match, 
+				and false if the IDs do match: toDos = toDos.filter(item => item.id !== id);
+				*/
+
+			/*	Inside the event listener for the delete button, 
+				call the deleteToDo function and pass it toDo.id.
+			Finally, inside the event listener, call renderTheUI() 
+			to update the UI.
+			*/
+
 			});
 		});
 	}
@@ -143,7 +179,7 @@ function onReady(){
 
 window.onload = function() {
   onReady();
-}
+};
 
 
 
